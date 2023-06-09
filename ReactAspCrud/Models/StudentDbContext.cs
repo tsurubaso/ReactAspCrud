b98f6fb1aaebac6cea3ed92cf024ae2c8cbe38a7
tsurubaso <tsurubaso@gmail.com>
2023-06-09 10:53:25 +0900

    backend finished

--- ReactAspCrud/Models/StudentDbContext.cs
+++ ReactAspCrud/Models/StudentDbContext.cs
@@ -1,8 +1,33 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
+
 namespace ReactAspCrud.Models
 {
     public class StudentDbContext : DbContext 
     {
+       
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Data Source=theok\\SQLEXPRESS;Initial Catalog=StudentService;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
